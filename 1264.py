0d954713f55a0a913d980371c7df7e46ad8e7cce
okSYS007 <46938447+okSYS007@users.noreply.github.com>
2021-12-19 00:19:33 +0200

    1254 Трудовые будни

---
+++ 1264.py
@@ -0,0 +1,13 @@
+_str = input().split()
+
+#N массив
+N = _str[0]
+#элемент
+M = _str[1]
+count = 0
+
+for i in range(N):
+    for j in range(M):
+        count += 1 if i = j else pass
+
+print('S')
