3ac9988f6fda30542222b78bbcb8a0b635ad5ce1
okSYS <ViacheslavDan803@gmail.com>
2021-12-23 22:35:07 +0200

    acc

--- 1197.py
+++ 1197.py
@@ -1,53 +1,32 @@
-#  |a|b|c|d|e|f|g|h|
-# 1| | | | | | | | |
-# 2| | | | | | | | |
-# 3| | | | | | | | |
-# 4| | | |x| | | | |
-# 5| | | | | | | | |
-# 6| | | | | | | | |
-# 7| | | | | | | | |
-# 8| | | | | | | | |
-
-N = int(input())
-_st1 = input()
-_st2 = input()
-_st3 = input()
-
-N = 3
+p =[
+    [2,3,4,4,4,4,3,2],
+    [3,4,6,6,6,6,4,3],
+    [4,6,8,8,8,8,6,4],
+    [4,6,8,8,8,8,6,4],
+    [4,6,8,8,8,8,6,4],
+    [4,6,8,8,8,8,6,4],
+    [3,4,6,6,6,6,4,3],
+    [2,3,4,4,4,4,3,2]
+    ]
 
 verbs_ = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 nums = [int(x)+1 for x in range(8)]
 
-# _st1 = 'a1'
-# _st2 = 'd4'
-# _st3 = 'g6'
-
-res = 0
-
+N = int(input())
+res = []
 for i in range(N):
 
-    verb = _st1[i]
-    num = int(i[1])+1
-    #индескы
-    #0
-    dot_x = verbs_.index(verb)+1
-    dot_y = nums.index(num)
+    _str1 = input()
+    x = _str1[0]
+    y = int(_str1[1])
+    dot_x = verbs_.index(x)
+    dot_y = nums.index(y)
+    
+    res.append(p[dot_x][dot_y])
+    
 
-    if dot_x == 1 or dot_x == 8:
-        res += 1
-    if dot_y == 1 or dot_y == 8:
-        res += 1
+for i in res:
+    print(i)
 
-    if dot_x <= 6 and dot_x >= 3:
-        if dot_x > 2 and dot_x < 5:
-            res += 4
-        else:
-            res += 3*2
-    if dot_y <= 6 and dot_y >= 3:
-        if dot_y > 2 and dot_y < 5:
-            res += 4
-        else:
-            res += 3*2
-
-    print(res)        
+     
 
